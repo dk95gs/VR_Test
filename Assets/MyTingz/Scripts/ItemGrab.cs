53df660b6ef8e8439bbc61da4a5b90ab52f15e7a
Your Name <you@example.com>
2018-12-27 23:26:19 -0500

    f

---
+++ Assets/MyTingz/Scripts/ItemGrab.cs
@@ -0,0 +1,21 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGrab : MonoBehaviour {
+
+    public Transform dest;
+
+    public void PickUp()
+    {
+    
+        this.transform.position = dest.position;
+        this.transform.parent = GameObject.Find("Hand").transform;
+		this.transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+    public void Drop()
+    {
+        this.transform.parent = null;
+        
+    }
+}
